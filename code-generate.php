c4d4906a7abb2a653ebd973a2d253bd8bfbefcb4
Achmad Fadhitya Muharram <afadhitya@gmail.com>
2020-08-23 10:27:16 +0700

    Change some structure

--- code-generate.php
+++ code-generate.php
@@ -8,12 +8,12 @@ $result = $conn->query($sql);
 $kodeBukuArr = array();
 
 while ($row = $result->fetch_assoc()) {
-    $kodeBuku = new KodeBuku();
-    $kodeBuku->kategori = $row['kategori'];
-    $kodeBuku->nomorKodeBuku = $row['nomor_kode_buku'];
-    $kodeBuku->inisialKodeBuku = $row['inisial_kode_buku'];
+  $kodeBuku = new KodeBuku();
+  $kodeBuku->kategori = $row['kategori'];
+  $kodeBuku->nomorKodeBuku = $row['nomor_kode_buku'];
+  $kodeBuku->inisialKodeBuku = $row['inisial_kode_buku'];
 
-    array_push($kodeBukuArr, $kodeBuku);
+  array_push($kodeBukuArr, $kodeBuku);
 }
 
 $dusParam=$_GET['dusParam'];
@@ -25,42 +25,42 @@ $count = 0;
 $arr = [];
 $i = 0;
 if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        $countStok = 0;
-        
-        while ($countStok < $row["jumlah"]){
-            $countKode = 0;
-            foreach ($kodeBukuArr as $kodeBukuObj){
-                if (strtolower(str_replace(' ', '', $row["kategori"])) == strtolower(str_replace(' ','', $kodeBukuObj->kategori))) {
-                    $arr[$i]['kategori'] = $kodeBukuObj->inisialKodeBuku;
-                    $arr[$i]['kode'] =  $kodeBukuObj->nomorKodeBuku;
-                    break;
-                } else {
-                    $countKode++;
-                }
-
-                if (sizeof($kodeBukuArr) == $countKode) {
-                    echo "kategori ". $row['kategori'] ." tidak terdaftar <br>" ;
-                } 
-            }  
-
-            $arr[$i]['judul_buku'] = strtoupper(substr($row["judul_buku"],0,1));
-            $arr[$i]['penulis'] = strtoupper(substr($row["penulis"],0,3));
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    $countStok = 0;
     
-            $i++;
-            $countStok++;
+    while ($countStok < $row["jumlah"]){
+      $countKode = 0;
+      foreach ($kodeBukuArr as $kodeBukuObj){
+        if (strtolower(str_replace(' ', '', $row["kategori"])) == strtolower(str_replace(' ','', $kodeBukuObj->kategori))) {
+          $arr[$i]['kategori'] = $kodeBukuObj->inisialKodeBuku;
+          $arr[$i]['kode'] =  $kodeBukuObj->nomorKodeBuku;
+          break;
+        } else {
+          $countKode++;
         }
+
+        if (sizeof($kodeBukuArr) == $countKode) {
+          echo "kategori ". $row['kategori'] ." tidak terdaftar <br>" ;
+        } 
+      }  
+
+      $arr[$i]['judul_buku'] = strtoupper(substr($row["judul_buku"],0,1));
+      $arr[$i]['penulis'] = strtoupper(substr($row["penulis"],0,3));
+
+      $i++;
+      $countStok++;
     }
+  }
 } else {
-    echo "0 results";
+  echo "0 results";
 }
 $conn->close();
 ?>
 
 <style>
 table, th, td {
-    border: 1px solid black;
+  border: 1px solid black;
 }
 </style>
 
@@ -69,32 +69,33 @@ $row = 0;
 $totalBuku = (sizeof($arr));
 
 while ($count < $totalBuku) {
-    if ( $row % 8 == 0 && $row != 0) echo "<br><br><br><br><br><br><br>";
+  if ( $row % 8 == 0 && $row != 0) echo "<br><br><br><br><br><br><br>";
 ?>
-    <table width="200" style="width:200mm;height:33mm; border-collapse: collapse;" >
-        <tr>
-            <?php 
-            for ($i = 0; $i < 5; $i++) {
-                    ?>
-                    <td align="center" width="148px">
-                        <?php
-                        if($count < $totalBuku){
-                            echo "<b>TIM PKM<br>POLBAN</b>" . "<br>";
-                            echo $arr[$count]['kategori'] . "<br>";
-                            echo $arr[$count]['kode'] . "<br>";
-                            echo $arr[$count]['penulis'] . "<br>";
-                            echo $arr[$count]['judul_buku'] . "<br>";
-                        }
-                            $count++;
-                        ?>
-                    </td>
-            <?php 
-            }
-
-            ?>
-        </tr>
-    </table>
+  <table width="200" style="width:200mm;height:33mm; border-collapse: collapse;" >
+    <tr>
+    <?php 
+    for ($i = 0; $i < 5; $i++) {
+    ?>
+      <td align="center" width="148px">
+      <?php
+      if ($count < $totalBuku) {
+        echo "<b>TIM PKM<br>POLBAN</b>" . "<br>";
+        echo $arr[$count]['kategori'] . "<br>";
+        echo $arr[$count]['kode'] . "<br>";
+        echo $arr[$count]['penulis'] . "<br>";
+        echo $arr[$count]['judul_buku'] . "<br>";
+      }
+      
+      $count++;
+      ?>
+      </td>
+    <?php 
+    }
+    ?>
+    </tr>
+  </table>
+  
 <?php 
-    $row++;
+$row++;
 } 
 ?>
--- import-excel.php
+++ import-excel.php
@@ -1,55 +1,72 @@
 <?php
-include "connection.php";
 
+include "connection.php";
 require "excel_reader.php";
 
 //jika tombol import ditekan
 if(isset($_POST['submit'])){
 
-    $target = basename($_FILES['databuku']['name']);
-    move_uploaded_file($_FILES['databuku']['tmp_name'], $target);
+  $target = basename($_FILES['databuku']['name']);
+  move_uploaded_file($_FILES['databuku']['tmp_name'], $target);
 
-// tambahkan baris berikut untuk mencegah error is not readable
-    chmod($_FILES['databuku']['name'],0777);
+  // tambahkan baris berikut untuk mencegah error is not readable
+  chmod($_FILES['databuku']['name'],0777);
 
-    $data = new Spreadsheet_Excel_Reader($_FILES['databuku']['name'],false);
+  $data = new Spreadsheet_Excel_Reader($_FILES['databuku']['name'],false);
 
-//    menghitung jumlah baris file xls
-    $baris = $data->rowcount($sheet_index=0);
+  // menghitung jumlah baris file xls
+  $baris = $data->rowcount($sheet_index=0);
 
-//    jika kosongkan data dicentang jalankan kode berikut
-    $drop = isset( $_POST["drop"] ) ? $_POST["drop"] : 0 ;
-    if($drop == 1){
-//             kosongkan tabel pegawai
-             $truncate ="TRUNCATE TABLE buku";
-             mysqli_query($conn, $truncate);
-    };
+  // jika kosongkan data dicentang jalankan kode berikut
+  $drop = isset( $_POST["drop"] ) ? $_POST["drop"] : 0 ;
+  if($drop == 1) {
+    // kosongkan tabel pegawai
+    $truncate ="TRUNCATE TABLE buku";
+    mysqli_query($conn, $truncate);
+  };
 
-//    import data excel mulai baris ke-2 (karena tabel xls ada header pada baris 1)
-    for ($i=2; $i<=$baris; $i++)
-    {
-//       membaca data (kolom ke-1 sd terakhir)
-      $judul  = $data->val($i, 2);
-      $kategori   = $data->val($i, 3);
-      $penulis = $data->val($i, 4);
-      $penerbit = $data->val($i, 5);
-      $tahun = $data->val($i, 6);
-      $jumlah = $data->val($i, 7);
-      $namaDus = $data->val($i, 8);
+  // import data excel mulai baris ke-2 (karena tabel xls ada header pada baris 1)
+  for ($i=2; $i<=$baris; $i++) {
+  // membaca data (kolom ke-1 sd terakhir)
+  $judul  = $data->val($i, 2);
+  $kategori   = $data->val($i, 3);
+  $penulis = $data->val($i, 4);
+  $penerbit = $data->val($i, 5);
+  $tahun = $data->val($i, 6);
+  $jumlah = $data->val($i, 7);
+  $namaDus = $data->val($i, 8);
 
-//      setelah data dibaca, masukkan ke tabel pegawai sql
-      $sql = "INSERT into buku (judul_buku,kategori,penulis,penerbit,tahun_terbit,jumlah, nama_dus)values
-      ('$judul','$kategori','$penulis','$penerbit','$tahun', '$jumlah', '$namaDus')";
-      if ($conn->query($sql) === TRUE) {
-          echo "Berhasil import <br>";
-      } else {
-          echo "Error: " . $sql . "<br>" . $conn->error;
-      }  
-    }
+  // setelah data dibaca, masukkan ke tabel pegawai sql
+  $sql = "INSERT into buku (judul_buku, kategori, penulis, penerbit,tahun_terbit,jumlah, nama_dus)values
+          (\"$judul\",\"$kategori\",\"$penulis\",\"$penerbit\",\"$tahun\", \"$jumlah\", \"$namaDus\")";
 
-//    hapus file xls yang udah dibaca
-    unlink($_FILES['databuku']['name']);
+  $errorFlag = 0;
+  $errorMessage;
+    if ($conn->query($sql) === TRUE) {
+      echo "Berhasil import <br>";
+    } else {
+      $errorMessage = "Error: " . $sql . "<br>" . $conn->error;
+      echo $errorMessage;
+      $errorFlag = 1;
+    }  
+  }
+
+  $status = $errorFlag == 1 ? "Gagal" : "Sukses";
+  $dateTime = date("Y-m-d H:i:s");
+  $remarks = $errorFlag == 1 ? $errorMessage : "";
+
+  $sql = "INSERT INTO import_history (tanggal, status, remarks, nama_dus) values 
+          (\"$dateTime\", \"$status\", \"$remarks\", \"$namaDus\")";
+
+  if ($conn->query($sql) === TRUE) {
+    echo "Berhasil save history";
+  } else {
+    $errorMessage = "Error: " . $sql . "<br>" . $conn->error;
+    echo $errorMessage;
+  }  
+
+  // hapus file xls yang udah dibaca
+  unlink($_FILES['databuku']['name']);
 }
 
-//
- ?>
+?>
